b52012059a5bdea3f3d9ca1cfebdd7654d38828b
Samir-Sahiti <dickthunder1345@gmail.com>
2026-04-03 12:15:25 +0200

    cleanup

--- backend/check_py_ast.js
+++
@@ -1,8 +0,0 @@
-const Parser = require('tree-sitter');
-const Python = require('tree-sitter-python');
-
-const parser = new Parser();
-parser.setLanguage(Python);
-const source = "class x: pass";
-const tree = parser.parse(source);
-console.log(tree.rootNode.toString());
